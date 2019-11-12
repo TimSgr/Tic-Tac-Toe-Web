abb92c5b1acf99c1ea02ea50c80906a4e6676495
Tim Seeger <tim.seeger@stackfuel.com>
2019-11-12 13:53:23 +0100

    Verschoben

--- www/Tic.php
+++ www/Tic.php
@@ -1,8 +1,5 @@
 <?php
 srand((double)microtime()*1000000);   
-
-
-
 function sieg($a,$spiel){
     if(
         $spiel[0][0]==$a && $spiel[0][1]==$a && $spiel[0][2]==$a || 
@@ -18,36 +15,58 @@ function sieg($a,$spiel){
        return null;
    }
 }
-
-
-
-
+function ki_next_move($spiel){
+    $ki_spiel=$spiel;
+           
+    for($e=0;$e<=2;$e++){
+        for($f=0;$f<=2;$f++){
+            if(($ki_spiel[$e][$f])!=null){
+                continue;
+            } 
+            $ki_spiel[$e][$f]="O";
+            $win=sieg("O",$ki_spiel);
+            if($win=="O"){
+                return $ki_spiel;
+            }
+            
+            $ki_spiel[$e][$f]=null;
+        }
+    }
+    while(true){                                                                                
+        $zufallszahl1 = rand(0, 8);
+        $spalte=intdiv($zufallszahl1, 3);
+        $zeile=$zufallszahl1%3;
+        //var_dump($spiel[$zeile][$spalte]);
+        if ($spiel[$spalte][$zeile]==null){ 
+            $spiel[$spalte][$zeile]="O"; 
+            //var_dump($spiel[$spalte][$zeile]);
+            break;
+        }    
+    }
+    return $spiel;
+}
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Tic Tac Toe (Web) </title>
-    <link rel="stylesheet" type="text/css" href="Tic3b.css">
-</head>
-<body>
+    <head>
+        <title>Tic Tac Toe (Web) </title>
+        <link rel="stylesheet" type="text/css" href="Tic3b.css">
+    </head>
+    <body>
 <?php
-if($game==0){
-    $x=0;
-    $y=0;
-}
+$x=$_POST["x"];
+$y=$_POST["y"];
+if($x==null)$x=0;
+if($y==null)$y=0;
 // Laden des letzen Spielstands
 $spiel = unserialize(base64_decode($_POST["1a"]));
-
 // Setzen des letzen Zugs
-
-
-if (isset($_POST['Neustart'])) unset($spiel);                                             // Wenn Nutzer den Neustart-Button klickt wird das Spielfeld zurückgesetzt
+if (isset($_POST['Neustart'])) unset($spiel);                            
        
-
-        echo "Spieler X ist an der Reihe";
+        echo "Spieler X ist an der Reihe<br>";
         
-        if (isset($_POST['A1'])&&$spiel[0][0]!="O")  {$spiel[0][0]="X" ;}                   // zweite bedingung nicht notwendig(entfernen?)
+        if (isset($_POST['A1'])&&$spiel[0][0]!="O")  {$spiel[0][0]="X" ;}                   
         if (isset($_POST['A2'])&&$spiel[0][1]!="O")  {$spiel[0][1]="X" ;}
         if (isset($_POST['A3'])&&$spiel[0][2]!="O")  {$spiel[0][2]="X" ;}
         if (isset($_POST['B1'])&&$spiel[1][0]!="O")  {$spiel[1][0]="X" ;}
@@ -57,59 +76,29 @@ if (isset($_POST['Neustart'])) unset($spiel);                                   
         if (isset($_POST['C2'])&&$spiel[2][1]!="O")  {$spiel[2][1]="X" ;}
         if (isset($_POST['C3'])&&$spiel[2][2]!="O")  {$spiel[2][2]="X" ;}
     
-    // Siegbedingungen für menschlichen Spieler
-    // in Funktion auslagern, da doppelt
     
     $sieger=sieg("X", $spiel);
     
     
-
-
 ?>
 <?php //echo "$zahl"; ?>
 <?php  ?>
-<form action="Tic3b.php" method="post"> 
+<form action="Tic.php" method="post"> 
 
 <?php 
     $game=1;
     
-    $leere_felder = [];
-    // foreach($spiel as $key => $reihe) {
-    //     foreach($reihe as $k => $field) {
-    //         if(empty($field)) {
-    //            $leere_felder[] ... 
-    //         }
-    //     }
-    // }
-                                                                                                    // leere Felder zählen und zufall davon auswählen
-while(true) {                                                                                       // KI hat 100 Versuche um ein Feld zu erwischen, was noch nicht belegt ist
-      $zufallszahl1 = rand(1,9);        
-      //$leere_felder = [1,2,3];
-//$leere_felder[rand(1,count($leere_felder)-1]                                                      // Es wird eine Zufallszahl erzeugt, die sich zwischen eins und neun befindet
-      if ($zufallszahl1==1 && $spiel[0][0]!="X" && $spiel[0][0]!="O"){ $spiel[0][0]="O"; break;}    // Diese Zufallszahl entspricht einer Position im Array bzw Spielfeld und das Programm überprüft
-      if ($zufallszahl1==2 && $spiel[0][1]!="X" && $spiel[0][1]!="O"){ $spiel[0][1]="O"; break;}    // ob diese Position schon belegt ist und wenn nicht wird als Wert des Arrays an der Position
-      if ($zufallszahl1==3 && $spiel[0][2]!="X" && $spiel[0][2]!="O"){ $spiel[0][2]="O"; break;}    // "O" gesetzt.
-      if ($zufallszahl1==4 && $spiel[1][0]!="X" && $spiel[1][0]!="O"){ $spiel[1][0]="O"; break;}    // Wenn das Feld schon belegt ist, erhält das Programm die Anweisung erneut zu würfeln
-      if ($zufallszahl1==5 && $spiel[1][1]!="X" && $spiel[1][1]!="O"){ $spiel[1][1]="O"; break;}    
-      if ($zufallszahl1==6 && $spiel[1][2]!="X" && $spiel[1][2]!="O"){ $spiel[1][2]="O"; break;}
-      if ($zufallszahl1==7 && $spiel[2][0]!="X" && $spiel[2][0]!="O"){ $spiel[2][0]="O"; break;}
-      if ($zufallszahl1==8 && $spiel[2][1]!="X" && $spiel[2][1]!="O"){ $spiel[2][1]="O"; break;}
-      if ($zufallszahl1==9 && $spiel[2][2]!="X" && $spiel[2][2]!="O"){ $spiel[2][2]="O"; break;}
-    }
+    $spiel=ki_next_move($spiel);
+                                                                                               
     //Siegbedingung für KI
     $sieger=sieg("X", $spiel);
     if (!$sieger){ 
         $sieger=sieg("O", $spiel);
     }
-// echo"<pre>";
-//     var_dump($sieger);
-//     echo"</pre>";
- 
     
     if($sieger=="X") { $game=0; }
     elseif($sieger=="O") { $game=0;}
     $message = null;
-
     if($sieger=="X"||$sieger=="O") {
         $message = $sieger ." hat gewonnen";
     }
@@ -119,27 +108,19 @@ while(true) {                                                                   
         elseif($sieger=="O"){
             $x++;
         }
-    // echo"<pre>";
-    // var_dump($game);
-    // echo"</pre>";
-    if($game==0) {                                                                                                // $game==0 beschreibt den Endzustand des Spiels, also wenn entweder ein Spieler gewonnen hat, oder das Spielfeld belegt ist
+   
+    if($game==0) {                                                                                                
         $disabled = "disabled";
     } else  {
         $disabled = "";
     }
-    // echo"<pre>";
-    // var_dump($disabled);
-    // echo"</pre>";
-    // ermittle, wenn es keinen Gewinner gibt
-
    
-
 ?>
 <!-- Senden des neuen Zugs -->
 
 <input type="submit" id="button01"  name="A1" value="<?php echo $spiel[0][0] ?>" <?php echo !empty($spiel[0][0]) ? "disabled" : $disabled ; ?>>
 <input type="submit" id="button02"  name="A2" value="<?php echo $spiel[0][1] ?>" <?php echo !empty($spiel[0][1]) ? "disabled" : $disabled ;?>>
-<input type="submit" id="button03" name="A3" value="<?php echo $spiel[0][2] ?>"  <?php echo !empty($spiel[0][2]) ? "disabled" : $disabled ;?>> <br> <br>
+<input type="submit" id="button03" name="A3" value="<?php echo $spiel[0][2] ?>"  <?php echo !empty($spiel[0][2]) ? "disabled" : $disabled ;?>> <br><br>
 <input type="submit" id="button04"  name="B1" value="<?php echo $spiel[1][0] ?>" <?php echo !empty($spiel[1][0]) ? "disabled" : $disabled ;?>>
 <input type="submit" id="button05"  name="B2" value="<?php echo $spiel[1][1] ?>" <?php echo !empty($spiel[1][1]) ? "disabled" : $disabled ;?>>
 <input type="submit" id="button06" name="B3" value="<?php echo $spiel[1][2] ?>"  <?php echo !empty($spiel[1][2]) ? "disabled" : $disabled ;?>> <br><br>
@@ -152,12 +133,11 @@ while(true) {                                                                   
 <input type="submit" name="Neustart" value="Neustart" id="button10">
 <br>
 <?php
-
 ?>
 <!-- Senden des letzten Spielstands -->
 <input type="hidden" name="1a" value="<?php echo base64_encode(serialize($spiel))?>">
-<input type="hidden" name="x" value="<?php $x ?>">
-<input type="hidden" name="y" value="<?php $y ?>">
+<input type="hidden" name="x" value="<?php echo $x ?>">
+<input type="hidden" name="y" value="<?php echo $y ?>">
 
     <?php 
     if(!empty($message)) {
